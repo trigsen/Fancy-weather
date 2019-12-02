f72098baa46ac36d68fb5fb38eb5f7182e9db064
Roman Triputko <yeahroma@gmail.com>
2019-12-02 17:26:51 +0300

    feat: apply webpack

---
+++ fancy-weather/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader"
+            ]
+        },
+        {
+            test: /\.(png|jpg)/,
+            use: 'url-loader'
+        }]
+    },
+    devServer: {
+        overlay: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ]
+}
